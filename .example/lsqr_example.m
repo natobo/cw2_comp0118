000c933674ab5c612a9163e95d4acd4376601b91
Camille Tyriard <camilletyriard@gmail.com>
2026-03-13 21:11:35 +0000

    restructure

--- .example/lsqr_example.m
+++ .example/lsqr_example.m
@@ -3,14 +3,18 @@ size(r.img)
 TE=load('TEs.txt')
 A=[ones(length(TE),1),TE'];
 r.img(r.img(:)<0)=0; % shouldn't be possible!
-s=log(r.img);
+% "Any voxel intensity that is below 0 is replaced by 0, because negative signal values are not physically expected."
+% s=log(r.img);
+s=log(double(r.img));
+
 new=A\reshape(s,numel(s)/length(TE),length(TE))';
 new(1,:)=exp(new(1,:));
+
 new(2,:)=-1./new(2,:);
 new=reshape(new',[size(s,1),size(s,2),size(s,3),2]);
 
-y=squeeze(sum(sum(nansum(r.img,1),2),3));
-y=y/max(y(:));
+y=squeeze(sum(sum(sum(r.img,1),2),3));
+y=y/max(y(:))
 
 % raw data...
 y=[1.000	0.983	0.964	0.955	0.932	0.928	0.903	0.863	0.817	0.780	0.748	0.716	0.671	0.634	0.606	0.566	0.504	0.433	0.376	0.327	0.330	0.247]';
@@ -41,3 +45,6 @@ for i=1:1000
     p2=lsqnonlin(@(p)(y-p(1)*exp(-TE(:)/p(2))),[exp(p(1)),T2],[],[],options);
 end
 disp(['1000 non-linear fits took ',num2str(toc),'s...'])
+
+
+%This figure is showing a T2 decay curve fit in a biomedical imaging context, most likely MRI relaxometry.
